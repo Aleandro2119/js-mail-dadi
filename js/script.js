3bd5bb4edc73aefbe35a9cb1ce522b6b986187c6
Aleandro2119 <aleandro.basilico@gmail.com>
2022-01-10 18:13:43 +0100

    Add array

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
-//Mail e Gioco dei dadi
+//Mail
 
 // # Mail
-/*Crea un array di email;
+/*
+Crea un array di email;
 Chiedi all’utente la sua email.
 controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.*/
 
+//1CREO UN ARRAY
+const emailList = ['aleandro.basilico@gmail.com', 'marco@gmail.com', 'giulia@gmail.com', 'alessio@gmail.com']
 
-// # Gioco dei dadi
-/*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Stampare in console i tiri di dado e il risultato.*/
+
+
+
 
 
