4386adadb98471defcb9629e2ef206e4333ff4b1
Aleandro2119 <aleandro.basilico@gmail.com>
2022-01-10 18:40:15 +0100

    Add user and pc

---
+++ js/script2.js
@@ -0,0 +1,14 @@
+//Gioco dei dadi
+
+/*Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampare in console i tiri di dado e il risultato.*/
+
+
+//1
+const userNumber = Math.floor(Math.random() * 7);
+console.log(userNumber);
+
+//2
+const userPc = Math.floor(Math.random() * 7);
+console.log(userPc);
